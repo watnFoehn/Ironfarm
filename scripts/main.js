9507f1ac3dcf696ee4799068c3ef119125008b71
watnfoehn <hoehnefelix@googlemail.com>
2018-10-23 13:43:21 +0200

    added movement, added a farm picture

--- scripts/main.js
+++ scripts/main.js
@@ -10,12 +10,24 @@ function DisableScrollbars()
 |    The constructor-class for the maze 
 |
 \-----------------------------------------------------------------------------------------*/
-function Game(maze, level) {
+function Game(maze) {
     this.maze = maze
-    this.level = level
+    this.player = {
+        y: 1,
+        x: 1,
+        //direction: "down"
+    };
 }
 
-var mazeGame = new Game ("", 1)
+var player = {
+    y: 1,
+    x: 1,
+}
+
+const YMAX = 20;
+const XMAX = 32;
+
+var mazeGame = new Game ("")
 
 
 
@@ -42,7 +54,7 @@ Game.prototype.makeMap = function () {
 
             //Adding CSS-classes to the different array-elements. 
             //For visuals AND gameplay-functionality (see jQuery section below)
-            if( this.maze[i][j] == "O" ) {
+            if( this.maze[i][j] == "0" ) {
                 $( "#" + i + "-" + j ).addClass( "game-area" );
 
                 //Adds visual borders only between "wall" and "game-area" divs
@@ -79,9 +91,9 @@ Game.prototype.makeMap = function () {
 6 = Ironbar found
 
 */
-var firstLevel = new Game([
+var gameworld = new Game([
     ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
-    ["*", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "*"],
+    ["*", "P", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "*"],
     ["*", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "*"],
     ["*", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "0", "*"],
     ["*", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "0", "*"],
@@ -102,71 +114,87 @@ var firstLevel = new Game([
     ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
     ]);
 
-    firstLevel.makeMap()
+    gameworld.makeMap()
 
 
-    /* Movement control */
 
-    var Keyboard = function() {
-        this.left = false;
-        this.right = false;
-        this.up = false;
-        this.down = false;
-        this.w = false;
-        this.s = false;
-        this.a = false;
-        this.d = false;
-    };
-    
-    // ASCII codes
-    var KEY_LEFT = 37;
-    var KEY_RIGHT = 39;
-    var KEY_UP = 38;
-    var KEY_DOWN = 40;
-    var KEY_W = 87;
-    var KEY_S = 83;
-    var KEY_A = 65;
-    var KEY_D = 68;
-    
-    var DIR_E = 1;
-    var DIR_NE = 2;
-    var DIR_N = 4;
-    var DIR_NW = 8;
-    var DIR_W = 16;
-    var DIR_SW = 32;
-    var DIR_S = 64;
-    var DIR_SE = 128;
-    
-    var isShift = false;
-    //var key = [false, false, false, false];
-    window.key = null;
-    
-    function InitializeKeyboard()
-    {
-        window.key = new Keyboard();
-    
-        $(document).keydown(function(e) {
-            if (e.keyCode == 16) isShift = true;
-            if (e.keyCode == KEY_LEFT) { key.left = true; }
-            if (e.keyCode == KEY_RIGHT) { key.right = true; }
-            if (e.keyCode == KEY_UP) { key.up = true; }
-            if (e.keyCode == KEY_DOWN) { key.down = true; }
-            if (e.keyCode == KEY_W) { key.w = true; }
-            if (e.keyCode == KEY_S) { key.s = true; }
-            if (e.keyCode == KEY_A) { key.a = true; }
-            if (e.keyCode == KEY_D) { key.d = true; }
-        });
-    
-        $(document).keyup(function(e) {
-            if (e.keyCode == 16) isShift = false;
-            if (e.keyCode == KEY_LEFT) { key.left = false; }
-            if (e.keyCode == KEY_RIGHT) { key.right = false; }
-            if (e.keyCode == KEY_UP) { key.up = false; }
-            if (e.keyCode == KEY_DOWN) { key.down = false; }
-            if (e.keyCode == KEY_W) { key.w = false; }
-            if (e.keyCode == KEY_S) { key.s = false; }
-            if (e.keyCode == KEY_A) { key.a = false; }
-            if (e.keyCode == KEY_D) { key.d = false; }
-        });
+
+
+var playerPiece = '<div id="player"></div>';
+$('.gameboard').append(playerPiece);
+
+
+$(window).on("keydown", function(evt) {
+    switch (evt.which) {
+      case 37:
+        console.log("left was called")
+        movePlayer("left");
+        
+        break;
+  
+      case 38:
+        movePlayer("up");
+        console.log("up was called")
+        break;
+        
+        case 39:
+        movePlayer('right');
+        console.log('right was called')
+        break;
+        
+        case 40:
+        movePlayer('down')
+        console.log('down was called')
+        break;
+        default:
+        console.log("invalid input");
     }
-    
+    $('#player').css({top: player.y *  22 + 'px'});
+    $('#player').css({left: player.x *  22 + 'px'});
+});
+
+
+
+  function movePlayer(direction) {
+    switch (direction) {
+      // LEFT LEFT LEFT LEFT
+      case "left":
+        if (player.x < 1) {
+          console.log("out of bounds");
+          break;
+        }
+        player.x -= 1;
+        console.log("X: " + player.x + "  Y: " + player.y);
+        break;
+      // UP UP UP UP
+      case "up":
+        if (player.y < 1) {
+          console.log("out of bounds");
+          break;
+        }
+        player.y -= 1;
+        console.log("X: " + player.x + "  Y: " + player.y);
+        break;
+      // RIGHT RIGHT RIGHT RIGHT
+      case 'right':
+        if (player.x < 1) {
+            console.log("out of bounds");
+          break;
+        }
+        player.x += 1;
+        console.log("X: " + player.x + "  Y: " + player.y);
+        break;
+      //DOWN DOWN DOWN DOWN
+      case 'down':
+      if (player.y < 1) {
+          console.log('out of bounds')
+          break;
+      }
+      player.y += 1;
+      console.log("X: " + player.x + "  Y: " + player.y);
+        break;
+    }
+  }
+
+  
+  
